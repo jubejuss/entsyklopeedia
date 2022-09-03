9e53618c447420038aca6d2bfe1070514d293ece
Juho Kalberg <juss@vilenkell.eu>
2022-09-03 12:42:48 +0300

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'Entsüklopeedia',
+  description: 'Siia kogun tähtsaid materjale',
+  themeConfig: {
+    nav: [
+      { text: 'Proge III', link: '/proge' },
+      { text: 'Configs', link: '/configs' },
+    ],
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+---
+title: Entsüklopeedia
+editLink: true
+---
+
+# {{ $frontmatter.title }}
